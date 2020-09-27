24499bf1b4933b548990cfb9a13127e464178540
pseudocodenerd <shekharmadhav03@gmail.com>
2020-09-27 15:19:12 +0530

    it just might have worked :flushed:

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use("/", indexRouter);
+// app.use("/", indexRouter);
 app.get("/api/test", (req, res) => {
   res.send("bonjour from the backend 🐼");
 });
